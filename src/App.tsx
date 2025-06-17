37db874aec7534216311db6cb27f8f17dd2781bc
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-17 17:16:48 -0300

    feat(layout): add content layout wrapper to App.tsx
    Introduces a `content` wrapper div in `App.tsx` and defines its styles in `_content.scss`.
    This wrapper provides a centered container with a maximum width, ensuring consistent
    layout for the main application content.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import '@styles/layouts/_content.scss'
+
 import { Banner } from './components/banner'
 
 function App() {
   return (
     <main>
       <Banner />
+
+      <div className="content"></div>
     </main>
   )
 }
