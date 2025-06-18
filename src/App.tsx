204cf6296d04f24adf9c6774df580b6a521802af
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-18 07:57:21 -0300

    feat(app): integrate Header component into App.tsx
    Places the `Header` component inside `content__tasks` div within `App.tsx`.
    This integrates the task statistics display into the main application layout.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import '@styles/layouts/_content.scss'
 
 import { Banner } from './components/banner'
 import { CreateForm } from './components/create-form'
+import { Header } from './components/header'
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
 
       <div className="content">
         <CreateForm />
+
+        <div className="content__tasks">
+          <Header />
+        </div>
       </div>
     </main>
   )
