02e09d0b4f2dcf400a55435d28230e2e4d087f88
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-18 19:47:58 -0300

    refactor(create-form): integrate task creation with TaskContext
    Refactors `CreateForm` to utilize `addTask` from `useTask` context for
    handling new task creation. Removes direct local storage interaction and
    `uuid` generation from the component, centralizing state management.

--- src/components/create-form.tsx
+++ src/components/create-form.tsx
@@ -2,7 +2,8 @@ import '@styles/components/_create-form.scss'
 
 import { PlusCircleIcon } from '@phosphor-icons/react'
 import { type FormEvent, useState } from 'react'
-import { v7 as uuid } from 'uuid'
+
+import { useTask } from '@/contexts/task-context'
 
 import {
   Select,
@@ -12,18 +13,23 @@ import {
   SelectValue,
 } from './ui/select'
 
-interface Task {
+interface TaskForm {
   description: string
   priority?: 'high' | 'regular' | 'low'
 }
 
 export function CreateForm() {
-  const [task, setTask] = useState<Task>({
+  const { addTask } = useTask()
+
+  const [task, setTask] = useState<TaskForm>({
     description: '',
     priority: undefined,
   })
 
-  function handleOnValueChange<F extends keyof Task>(field: F, value: Task[F]) {
+  function handleOnValueChange<F extends keyof TaskForm>(
+    field: F,
+    value: TaskForm[F],
+  ) {
     setTask((previous) => ({
       ...previous,
       [field]: value,
@@ -34,14 +40,10 @@ export function CreateForm() {
     event.preventDefault()
 
     if (task.description.trim() && task.priority) {
-      const currentTasks = JSON.parse(localStorage.getItem('tasks') ?? '[]')
-      const newTask = {
-        id: uuid(),
-        ...task,
-      }
-
-      currentTasks.push(newTask)
-      localStorage.setItem('tasks', JSON.stringify(currentTasks))
+      addTask({
+        description: task.description,
+        priority: task.priority,
+      })
 
       setTask({
         description: '',
@@ -65,9 +67,9 @@ export function CreateForm() {
 
       <Select
         name="priority"
-        value={task?.priority}
+        value={task.priority ?? ''}
         onValueChange={(value) =>
-          handleOnValueChange('priority', value as Task['priority'])
+          handleOnValueChange('priority', value as TaskForm['priority'])
         }
       >
         <SelectTrigger>
