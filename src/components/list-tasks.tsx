bce9ab9308ad767ecf4d54be3fddf11244d2f9cd
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-18 19:48:28 -0300

    feat(list-tasks): render dynamic task list from context
    Refactors `ListTasks` component to dynamically render `TaskCard` components
    based on the `tasks` array provided by `useTask` context.
    Implements conditional rendering to show the empty state when no tasks exist.

--- src/components/list-tasks.tsx
+++ src/components/list-tasks.tsx
@@ -2,22 +2,30 @@ import '@styles/components/_list-tasks.scss'
 
 import { ClipboardTextIcon } from '@phosphor-icons/react'
 
+import { useTask } from '@/contexts/task-context'
+
 import { TaskCard } from './task-card'
 
 export function ListTasks() {
+  const { tasks } = useTask()
+
   return (
     <div className="list">
-      <div className="list__content">
-        <TaskCard />
-      </div>
-
-      <div className="list__empty">
-        <ClipboardTextIcon size={56} />
-        <span>
-          <strong>Você ainda não tem tarefas cadastradas</strong> <br />
-          Crie tarefas e organize seus itens a fazer
-        </span>
-      </div>
+      {tasks.length > 0 ? (
+        <div className="list__content">
+          {tasks.map((task) => (
+            <TaskCard key={task.id} task={task} />
+          ))}
+        </div>
+      ) : (
+        <div className="list__empty">
+          <ClipboardTextIcon size={56} />
+          <span>
+            <strong>Você ainda não tem tarefas cadastradas</strong> <br />
+            Crie tarefas e organize seus itens a fazer
+          </span>
+        </div>
+      )}
     </div>
   )
 }
