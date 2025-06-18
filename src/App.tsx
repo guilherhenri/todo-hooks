c84fc7c84053ce01fb0fde15b8c20b7b8a9ecda4
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-18 09:14:15 -0300

    feat(app): integrate ListTasks component into App.tsx
    Adds the `ListTasks` component within the `content__tasks` div in `App.tsx`.
    This completes the integration of the task listing section into the main layout.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@styles/layouts/_content.scss'
 import { Banner } from './components/banner'
 import { CreateForm } from './components/create-form'
 import { Header } from './components/header'
+import { ListTasks } from './components/list-tasks'
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
 
         <div className="content__tasks">
           <Header />
+
+          <ListTasks />
         </div>
       </div>
     </main>
