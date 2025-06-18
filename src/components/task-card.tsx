7958d90e447e94300f3616737024b86708500901
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-18 19:48:50 -0300

    feat(task-card): implement task update and delete via context
    - Enhances `TaskCard` to receive `task` object as prop.
    - Integrates `updateTask` and `deleteTask` from `useTask` context.
    - Implements `handleToggleTask` to update `isDone` status via `updateTask`.
    - Connects delete button to `deleteTask` function.
    - Adds styling (`_task-card.scss`) for completed tasks (line-through effect).
    - Includes `translatePriority` helper for displaying priority in Portuguese.

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -2,22 +2,56 @@ import '@styles/components/_task-card.scss'
 
 import { PencilIcon, TrashIcon } from '@phosphor-icons/react'
 
+import { type Task, useTask } from '@/contexts/task-context'
+
 import { Checkbox } from './ui/checkbox'
 
-export function TaskCard() {
+export function TaskCard({
+  task: { id, description, priority, isDone },
+}: {
+  task: Task
+}) {
+  const { updateTask, deleteTask } = useTask()
+
+  function translatePriory(priority: Task['priority']) {
+    switch (priority) {
+      case 'high':
+        return 'Alta'
+      case 'regular':
+        return 'Normal'
+      case 'low':
+        return 'Baixa'
+    }
+  }
+
+  function handleToggleTask(state: boolean) {
+    updateTask({
+      id,
+      description,
+      priority,
+      isDone: state,
+    })
+  }
+
   return (
     <div className="card">
       <div className="card__done">
-        <Checkbox />
+        <Checkbox
+          defaultChecked={isDone}
+          onCheckedChange={(checked) => handleToggleTask(Boolean(checked))}
+        />
       </div>
 
       <div className="card__info">
-        <p className="card__info-title">
-          Integer urna interdum massa libero auctor neque turpis turpis semper.
-          Duis vel sed fames integer.
+        <p className="card__info-description" data-done={isDone}>
+          {description}
         </p>
-        <span className="card__info-priority" data-priority="high">
-          Alta
+        <span
+          className="card__info-priority"
+          data-done={isDone}
+          data-priority={priority}
+        >
+          {translatePriory(priority)}
         </span>
       </div>
 
@@ -28,7 +62,10 @@ export function TaskCard() {
 
         <span className="card__actions-separator" />
 
-        <button className="card__actions-button card__actions-delete">
+        <button
+          className="card__actions-button card__actions-delete"
+          onClick={() => deleteTask(id)}
+        >
           <TrashIcon size={18} />
         </button>
       </div>
