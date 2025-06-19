7748fc8ee3cb1c5fd76ec52953441c5902842aef
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-19 11:09:15 -0300

    feat(task): implement EditForm component
    - Creates `EditForm` component (`src/components/edit-form.tsx`) for editing
      existing tasks.
    - Utilizes `useState`, `useEffect`, and `useMemo` for internal state management,
      data loading, and form validation.
    - Integrates `Select` component for priority selection.
    - Styles the form (`_edit-form.scss`), including input, select, and submit button.
    - Displays a loading state while fetching task data.
    - Handles form submission by calling `updateTask` from `TaskContext`.

---
+++ src/components/edit-form.tsx
@@ -0,0 +1,105 @@
+import '@styles/components/_edit-form.scss'
+
+import { type FormEvent, useEffect, useMemo, useState } from 'react'
+
+import { type Task, useTask } from '@/contexts/task-context'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
+
+export function EditForm({
+  id,
+  closeDialog,
+}: {
+  id: string
+  closeDialog: () => void
+}) {
+  const { getTask, updateTask } = useTask()
+
+  const [task, setTask] = useState<Task | undefined>()
+
+  function handleOnValueChange<F extends keyof Task>(field: F, value: Task[F]) {
+    setTask((previous) => {
+      if (previous) {
+        return {
+          ...previous,
+          [field]: value,
+        }
+      }
+    })
+  }
+
+  function handleUpdateTask(event: FormEvent) {
+    event.preventDefault()
+
+    if (task) {
+      updateTask(task)
+      closeDialog()
+    }
+  }
+
+  useEffect(() => {
+    const data = getTask(id)
+
+    setTask(data)
+  }, [id, getTask])
+
+  const isFormValid = useMemo(() => {
+    if (task) {
+      return task.description.trim() && task.priority
+    }
+
+    return false
+  }, [task])
+
+  if (!task) {
+    return (
+      <div className="edit-form__load">
+        <span>Carregando...</span>
+      </div>
+    )
+  }
+
+  return (
+    <form className="edit-form" onSubmit={handleUpdateTask}>
+      <input
+        className="edit-form__input"
+        type="text"
+        name="description"
+        placeholder="Adicione uma nova tarefa"
+        value={task.description}
+        onChange={(e) => handleOnValueChange('description', e.target.value)}
+      />
+
+      <Select
+        name="priority"
+        value={task.priority}
+        onValueChange={(value) =>
+          handleOnValueChange('priority', value as Task['priority'])
+        }
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Prioridade" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="high">Alta</SelectItem>
+          <SelectItem value="regular">Normal</SelectItem>
+          <SelectItem value="low">Baixa</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <button
+        className="edit-form__submit-button"
+        type="submit"
+        disabled={!isFormValid}
+      >
+        Atualizar
+      </button>
+    </form>
+  )
+}
