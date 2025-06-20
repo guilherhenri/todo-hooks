fc7c7055b678d252fa0875307f77ae7b74478854
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-19 21:39:26 -0300

    test(alias): configurate @ alias to src

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/src/setup-tests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
